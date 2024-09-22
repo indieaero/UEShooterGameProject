c2716f63f768a25b4572f8dba26610317d442669
kabirov.a <artemkabirov.am@gmail.com>
2024-09-22 16:21:30 +0400

    MakeShot refactoring

--- Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
@@ -25,8 +25,6 @@ void ASTUBaseWeapon::BeginPlay()
 
 void ASTUBaseWeapon::Fire()
 {
-    UE_LOG(LogBaseWeapon, Display, TEXT("Fire!"));
-
     MakeShot();
 }
 
@@ -34,48 +32,78 @@ void ASTUBaseWeapon::MakeShot()
 {
     if (!GetWorld()) return;
 
+    FVector TraceStart, TraceEnd;
+    if (!GetTraceData(TraceStart, TraceEnd)) return;
+
+    FHitResult HitResult;
+
+    MakeHit(HitResult, TraceStart, TraceEnd);
+
+    if (HitResult.bBlockingHit)
+    {
+        // Draw debug line from the TraceStart to the TraceEnd for visualization in the world.
+        DrawDebugLine(GetWorld(), GetMuzzleWorldLocation(), HitResult.ImpactPoint, FColor::Red, false, 3.0f, 0, 3.0f);
+
+        DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10.0f, 24, FColor::Red, false, 5.0f);
+    }
+    else
+    {
+        DrawDebugLine(GetWorld(), GetMuzzleWorldLocation(), TraceEnd, FColor::Red, false, 3.0f, 0, 3.0f);
+    }
+}
+
+APlayerController* ASTUBaseWeapon::GetPlayerController() const
+{
     const auto Player = Cast<ACharacter>(GetOwner());
-    if (!Player) return;
+    if (!Player) return nullptr;
 
-    const auto Controller = Player->GetController<APlayerController>();
-    if (!Controller) return;
+    return Player->GetController<APlayerController>();
+}
 
+bool ASTUBaseWeapon::GetPlayerViewPoint(FVector& ViewLocation, FRotator& ViewRotation) const
+{
+    const auto Controller = GetPlayerController();
+    if (!Controller) return false;
+
+    // TODO: see the insides of this function
+    Controller->GetPlayerViewPoint(ViewLocation, ViewRotation);
+    return true;
+}
+
+FVector ASTUBaseWeapon::GetMuzzleWorldLocation() const
+{
+    return WeaponMesh->GetSocketLocation(MuzzleSocketName);
+}
+
+bool ASTUBaseWeapon::GetTraceData(FVector& TraceStart, FVector& TraceEnd) const
+{
     FVector ViewLocation;
     FRotator ViewRotation;
 
-    //TODO: see the insides of this function
-    Controller->GetPlayerViewPoint(ViewLocation, ViewRotation);
+    if(!GetPlayerViewPoint(ViewLocation, ViewRotation)) return false;
 
     // Get the position and orientation of the socket (attachment point on the weapon model) for the muzzle (MuzzleSocketName)
-    const FTransform SocketTransform = WeaponMesh->GetSocketTransform(MuzzleSocketName);
+    //const FTransform SocketTransform = WeaponMesh->GetSocketTransform(MuzzleSocketName);
 
     // Set the starting point of the trace (TraceStart) to the socket's location (the muzzle)
-    const FVector TraceStart = ViewLocation; // SocketTransform.GetLocation();
+    TraceStart = ViewLocation; // SocketTransform.GetLocation();
 
     // Calculate the shooting direction based on the socket's orientation
     const FVector ShootDirection = ViewRotation.Vector();  // SocketTransform.GetRotation().GetForwardVector();
 
     // Calculate the end point of the trace
-    const FVector TraceEnd = TraceStart + ShootDirection * TraceMaxDistance;
+    TraceEnd = TraceStart + ShootDirection * TraceMaxDistance;
+    return true;
+}
 
-    //This function takes an actor pointer and adds it to the array of actors to ignore during the trace, allowing to ignore our character
+void ASTUBaseWeapon::MakeHit(FHitResult& HitResult, const FVector& TraceStart, const FVector& TraceEnd)
+{
+    if (!GetWorld()) return;
+
+    // This function takes an actor pointer and adds it to the array of actors to ignore during the trace, allowing to ignore our character
     FCollisionQueryParams CollisionParams;
     CollisionParams.AddIgnoredActor(GetOwner());
 
-    //Perform a line trace to detect a hit along the shot path, using the LineTraceSingleByChannel
-    FHitResult HitResult;
+    // Perform a line trace to detect a hit along the shot path, using the LineTraceSingleByChannel
     GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ECollisionChannel::ECC_Visibility, CollisionParams);
-
-    if (HitResult.bBlockingHit)
-    {
-        // Draw debug line from the TraceStart to the TraceEnd for visualization in the world.
-        DrawDebugLine(GetWorld(), SocketTransform.GetLocation(), HitResult.ImpactPoint, FColor::Red, false, 3.0f, 0, 3.0f);
-
-        DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10.0f, 24, FColor::Red, false, 5.0f);
-
-        UE_LOG(LogBaseWeapon, Display, TEXT("Bone: %s"), *HitResult.BoneName.ToString());
-    }else
-    {
-        DrawDebugLine(GetWorld(), SocketTransform.GetLocation(), TraceEnd, FColor::Red, false, 3.0f, 0, 3.0f);
-    }
 }
