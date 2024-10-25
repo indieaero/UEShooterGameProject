c72c6c1efde641ee76710695ac071cd64bbb11fc
kabirov.a <artemkabirov.am@gmail.com>
2024-10-25 23:54:18 +0400

    CrossHair Icon for each type of weapon

--- Source/ShootThemUp/Private/UI/STUPlayerHUDWidget.cpp
+++ Source/ShootThemUp/Private/UI/STUPlayerHUDWidget.cpp
@@ -2,20 +2,33 @@
 
 #include "UI/STUPlayerHUDWidget.h"
 #include "Components/STUHealthComponent.h"
+#include "Components/STUWeaponComponent.h"
 #include "GameFramework/Pawn.h"
 
 float USTUPlayerHUDWidget::GetHealthPercent() const
 {
     //variable Player is created, which stores a reference to the game character that owns this HUD widget
     const auto Player = GetOwningPlayerPawn();
-    if (!Player) return 0.0;
+    if (!Player) return 0.0f;
 
     //Using GetComponentByClass attempts to get the USTUHealthComponent component from the player class (Pawn). A pointer to the component is returned if it exists.
     const auto Component = Player->GetComponentByClass(USTUHealthComponent::StaticClass());
     const auto HealthComponent = Cast<USTUHealthComponent>(Component);
-
     if (!HealthComponent) return 0.0f;
 
     return HealthComponent->GetHealthPercent();
 
+}
+
+bool USTUPlayerHUDWidget::GetWeaponUIData(FWeaponUIData& UIData) const
+{
+    const auto Player = GetOwningPlayerPawn();
+    if (!Player) return false;
+
+    const auto Component = Player->GetComponentByClass(USTUWeaponComponent::StaticClass());
+    const auto WeaponComponent = Cast<USTUWeaponComponent>(Component);
+
+    if (!WeaponComponent) return 0.0f;
+
+    return WeaponComponent->GetWeaponUIData(UIData);
 }
