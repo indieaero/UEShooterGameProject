4b603f3bf5b1ba386512d5f5b9a5b6fabd45cd5f
kabirov.a <artemkabirov.am@gmail.com>
2024-10-25 20:06:52 +0400

    Character Health Bar: Blueprint widget

--- Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -17,6 +17,8 @@ void USTUHealthComponent::BeginPlay()
 {
     Super::BeginPlay();
 
+    check(MaxHealth > 0);
+
     // Every time when game is start
     SetHealth(MaxHealth);
 
--- Source/ShootThemUp/Public/Components/STUHealthComponent.h
+++ Source/ShootThemUp/Public/Components/STUHealthComponent.h
@@ -23,6 +23,9 @@ public:
     UFUNCTION(BlueprintCallable, Category = "Health")
     bool IsDead() const { return FMath::IsNearlyZero(Health); }
 
+    UFUNCTION(BlueprintCallable, Category = "Health")
+    float GetHealthPercent() const { return Health / MaxHealth; }
+
     //Create a function for the Health variable that will return its value
     float GetHealth() const { return Health; }
 
