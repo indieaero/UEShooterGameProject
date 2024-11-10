f5ad817489d7a16edd49a375581f2bd99d147c3f
kabirov.a <artemkabirov.am@gmail.com>
2024-11-10 22:04:45 +0400

    Added BasePickup, HealthPickup, AmmoPickup classes and static meshes for each pickup

---
+++ Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
@@ -0,0 +1,35 @@
+// Shoot Them Up Game, All Rights Reserved.
+
+#include "Pickups/STUBasePickup.h"
+#include "Components/SphereComponent.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogBasePickup, All, All)
+
+ASTUBasePickup::ASTUBasePickup()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    CollisionComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
+    CollisionComponent->InitSphereRadius(50.0f);
+    CollisionComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    CollisionComponent->SetCollisionResponseToAllChannels(ECR_Overlap);
+    SetRootComponent(CollisionComponent);
+}
+
+void ASTUBasePickup::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void ASTUBasePickup::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+    Super::NotifyActorBeginOverlap(OtherActor);
+
+    UE_LOG(LogBasePickup, Display, TEXT("Pickup was taken"));
+    Destroy();
+}
+
+void ASTUBasePickup::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
