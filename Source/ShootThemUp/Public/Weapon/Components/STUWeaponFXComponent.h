b8e6d4226ce07d9d4353fd13a65f0fcde9c818bf
kabirov.a <artemkabirov.am@gmail.com>
2024-12-11 20:57:23 +0400

    VFX: Spawn bullet marks decals

--- Source/ShootThemUp/Public/Weapon/Components/STUWeaponFXComponent.h
+++ Source/ShootThemUp/Public/Weapon/Components/STUWeaponFXComponent.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "STUCoreTypes.h"
 #include "Components/ActorComponent.h"
 #include "STUWeaponFXComponent.generated.h"
 
@@ -21,8 +22,8 @@ public:
 
 protected:
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
-    UNiagaraSystem* DefaultEffect;
+    FImpactData DefaultImpactData;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
-    TMap<UPhysicalMaterial*, UNiagaraSystem*> EffectsMap;
+    TMap<UPhysicalMaterial*, FImpactData> ImpactDataMap;
 };
