3ae6177af75ef3d5b1b9eb8c311035452b7bf031
kabirov.a <artemkabirov.am@gmail.com>
2025-03-03 16:18:50 +0100

    AI: Translating behavior tree launch logic into code + setting up smoother character turns

--- Source/ShootThemUp/Private/AI/STUAICharacter.cpp
+++ Source/ShootThemUp/Private/AI/STUAICharacter.cpp
@@ -2,10 +2,19 @@
 
 #include "AI/STUAICharacter.h"
 #include "AI/STUAIController.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 
 ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit) : Super(ObjInit)
 {
     AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
     AIControllerClass = ASTUAIController::StaticClass();
+
+    //Setting up smoother character turns
+    bUseControllerRotationYaw = false;
+    if (GetCharacterMovement())
+    {
+        GetCharacterMovement()->bUseControllerDesiredRotation = true;
+        GetCharacterMovement()->RotationRate = FRotator(0.0f, 200.0f, 0.0f);
+    }
 }
