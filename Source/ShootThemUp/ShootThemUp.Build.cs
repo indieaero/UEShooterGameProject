5f776cc1b0bba118107f5e87c07fe392f732e117
Artem Kabirov <artemkabirov.am@gmail.com>
2025-05-11 19:08:15 +0200

    AI: Context. New Generator: generation of points around the enemy, move to one when the enemy is detected

--- Source/ShootThemUp/ShootThemUp.Build.cs
+++ Source/ShootThemUp/ShootThemUp.Build.cs
@@ -36,6 +36,7 @@ public class ShootThemUp : ModuleRules
 			"ShootThemUp/Public/AI",
 			"ShootThemUp/Public/AI/Tasks",
 			"ShootThemUp/Public/AI/Services",
+			"ShootThemUp/Public/AI/EQS",
 		});
 
 		// Uncomment if you are using Slate UI
