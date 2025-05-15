8880e9ccab53b9a78251d3444c44889dc5768dff
Artem Kabirov <artemkabirov.am@gmail.com>
2025-05-15 21:29:46 +0200

    AI: EQS Find ammo pickups, AmmoNeed Decorator, update BT logic

--- Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -32,6 +32,7 @@ public:
 
     bool TryToAddAmmo(int32 ClipsAmount);
     bool IsAmmoEmpty() const;
+    bool IsAmmoFull() const;
 
 protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
@@ -68,7 +69,6 @@ protected:
 
     void DecreaseAmmo();
     bool IsClipEmpty() const;
-    bool IsAmmoFull() const;
 
     void LogAmmo();
     
