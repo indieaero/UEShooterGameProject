33d44f698f87f50e5665af0db3e5d087b375e590
Artem Kabirov <artemkabirov.am@gmail.com>
2026-01-19 20:02:35 +0100

    Menu: Add GameInstance for startup level configuration
    - Add STUGameInstance with StartUpLevelName property
    - Refactor STUMenuWidget to use level from GameInstance
    - Configure GameInstance class in project settings

---
+++ Source/ShootThemUp/Public/STUGameInstance.h
@@ -0,0 +1,20 @@
+// Shoot Them Up Game, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "STUGameInstance.generated.h"
+
+UCLASS()
+class SHOOTTHEMUP_API USTUGameInstance : public UGameInstance
+{
+    GENERATED_BODY()
+
+public:
+    FName GetStartUpLevelName() const { return StartUpLevelName; }
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    FName StartUpLevelName = NAME_None;
+};
